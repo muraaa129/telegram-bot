866d62ba0c6c89f8a10488b684e53ef772fef4ae
Murat <muratyusupkhanov@gmail.com>
2026-08-12 23:11:37 +0300

    добавил письма и их логику с месячной отправкой, затем закончил проект

--- main.js
+++ main.js
@@ -1,32 +1,55 @@
 import 'dotenv/config';
 import { Bot } from 'grammy';
 import { anniversary } from './morefiles/dates.js';
-import { calculateStats, calculateDays } from './morefiles/dateCalculator.js';
-import { getDaysMessage, getStatsMessage } from './morefiles/message.js';
+import * as calc from './morefiles/dateCalculator.js';
+import * as messages from './morefiles/message.js';
 
 const TOKEN = process.env.BOT_TOKEN;
 const bot = new Bot(TOKEN);
+const CHAT_ID = -4636532568;
 
 bot.command('stats', async(ctx) => {
-    const stats = calculateStats(anniversary);
-    const text = getStatsMessage(stats);
+    const stats = calc.calculateStats(anniversary);
+    const text = messages.getStatsMessage(stats);
 
     await ctx.reply(text, {
         reply_parameters: {
             message_id: ctx.msg.message_id
         }
     })
-})
+});
 
 bot.command('days', async(ctx) => {
-    const days = calculateDays(anniversary);
-    const text = getDaysMessage(days);
+    const days = calc.calculateDays(anniversary);
+    const text = messages.getDaysMessage(days);
 
     await ctx.reply(text, {
         reply_parameters: {
             message_id: ctx.msg.message_id
         }
     })
-})
+});
+
+let hasSentToday = false; // checker if message is sent or not
+setInterval(async () => {
+    try {
+        const isTargetDay = messages.checkMonthlyMessage(); // value true or false
+
+        if(isTargetDay === true && hasSentToday === false) {
+            await bot.api.sendMessage(CHAT_ID, messages.getMonthlyMessage(calc.calculateStats(anniversary)));
+            hasSentToday = true;
+            // if today is 13th of the month and message isn't sent yet
+            // then it will send to the group in telegram, then 
+            // checker's value becomes true to prevent sending another messages every 60 seconds
+        } else if (isTargetDay === false) {
+            hasSentToday = false;
+            // if today is not 13th, resets the checker to false
+        }
+
+    } catch(error) {
+        console.error('Error: ', error);
+    }
+}, 60000); // it is in Milliseconds, so 60 seconds
+
 
 bot.start();
--- morefiles/message.js
+++ morefiles/message.js
@@ -1,7 +1,22 @@
+import { letters, getRandomLetter } from './letters.js'
+
 export function getStatsMessage(stats) {
     return `Біз ${stats.years} жыл, ${stats.months} ай және ${stats.days} күн біргеміз, Жаным! ❤️`
 }
 
 export function getDaysMessage(days) {
     return `Біз бірге болғанымызға ${days} күн болды, Жаным! ❤️`
+}
+
+export function getMonthlyMessage(stats) {
+    const letter = getRandomLetter(letters)
+
+    return `Біз ${stats.years} жыл, ${stats.months} ай және ${stats.days} күн біргеміз, Жаным! ❤️\n\n${letter}`
+}
+
+export function checkMonthlyMessage(checkToday = new Date()) {
+    const targetDay = 13;
+    const today = checkToday.getDate(); // .getDate() takes only DAY from object Date
+
+    return today === targetDay // "===" always returns either true or false
 }
